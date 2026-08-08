8c7861c5e350720b9760b4554b0d43fbf28152d7
Julaly <mtm296185@gmail.com>
2026-08-08 14:49:33 +0700

    coin

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -6,6 +6,12 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TMP_Text notiText;
 
+    [SerializeField]
+    private GameObject restartButton;
+
+    [SerializeField]
+    private Player player;
+
     public static UIManager Instance;
 
     void Awake()
@@ -29,6 +35,20 @@ public class UIManager : MonoBehaviour
     public void ShowNotiText(string s)
     {
         notiText.text = s;
+    }
+
+    public void RestartGame()
+    {
+        player.transform.position = new Vector3(0f, 88f, -86f);
+        player.HP = 100;
+        ShowNotiText("Restart kub auan");
+        Time.timeScale = 1f;
+        ShowHideRestartButton(false);
+    }
+
+    public void ShowHideRestartButton(bool flag)
+    {
+        restartButton.SetActive(flag);
     }
 
 }
